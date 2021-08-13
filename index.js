85baf6c202d0454d4572b204821fd320b3ef58f7
Felipe <88596443+felipenferreira@users.noreply.github.com>
2021-08-13 17:04:23 +0000

    "Atualizações da Aula 4"
    - Cópia com os códigos feitos pelo prof. da Aula 4, preciso ajeitá-los pra fazer a aplicação ter a minha cara.

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ const app = express();
 const routes = require("./routes");
 //antes tava no routes,js mas veio pro index.js
 const expressLayouts = require("express-ejs-layouts");
+const { urlencoded } = require("express");
 
 const port = process.env.PORT || 3000;
 // const address = "localhost";
 // o local host é o nome padrão do computador e está ligado ao IP 127.0.0.1 (tcp-ip)
+
+// a palavra reservada global cria uma variável ou objeto global para o sistemas. Ele pode ser visto em qualquer parta do código ou dos módulos do projeto. Assim, Users podem ser vistos tanto aqui no index.js quanto em routes.js
+global.users =[
+    {name:"Wellington W. F. Sarmento",address:"Rua Dom Jeronimo, 666",email:"wwagner@virtual.ufc.br",age:46,height:1.70,vote:true},
+    {name:"PAtricia S. Paula",address:"Rua Dom Jeronimo, 666",email:"patricia@virtual.ufc.br",age:46,height:1.70,vote:true},
+    {name:"Henrique Sérgio L. Pequeno",address:"Rua do Henrique, 666",email:"henrique@virtual.ufc.br",age:46,height:1.70,vote:true}];
 
 // ativa o uso do EJS do express-ejs-layouts
 // antes estava no routes.js mas foi movido pro index.js
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
+app.use(express.urlencoded({extended:false})); //prepara a aplicacao para receber dados na forma de query string
+app.use(express.json()); //prepara a aplicacao para receber dados no formato JSON
+
+//Criando usando rotas simples que estão no arquivo routes.js
 app.use ('/', routes);
 
 const server = app.listen(port, () => {
--- routes.js
+++ routes.js
@@ -9,51 +9,49 @@ let db = require("./db");
 db.criarDB("minhaBaseDados");
 */
 
-/* 1. Especifica a pasta contendo arquivos estáticos;
-   2. O nome 'public' não precisará ser colocado na rota; 3. Para serem alcançados os arquivos e pastas que estão dentro dele; 
-   4. Por isso na imagem que está na página home.ejs só há o indicativo para 'images';
-*/
 router.use(express.static('public'));
 
-router.get('/',(req,res)=>{
-    res.render('pages/home');
-    // forma não elegante: res.send
+router.get('/', (req, res) => { //callback - função que trata dado evento GET
+  res.render('pages/home');
 });
 
-router.get('/about',(req,res)=>{
-    res.render('pages/about',{usuarios});
+router.get('/about', (req, res) => { //callback - função que trata dado evento GET
+  res.render('pages/about');
+});
+
+router.get('/cadastro', (req, res) => { //callback - função que trata dado evento GET
+  res.render('pages/cadastro', { users: users });
 });
 
 // Remover um usuário
-router.post('/cadastro/remove',(req,res)=>{
-    let usuario={name: "Felipe", email: "felipenferreiraa@gmail.com"};
-    /* for(let cont=1;cont<=6;cont++){
-        usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
-    } */
+router.post('/cadastro/remove', (req, res) => {
+  let item = req.body.id; //pega o valor passado através do parâmetro id e atribui a variável item. 
 
-    let result = db.inserirDado(usuario);
-    console.log(result);
-    //res.render('pages/insert',{result});
-});
-
-router.get('/cadastro',(req,res)=>{
-    res.render('pages/cadastro');
+  users.splice(item, 1); //este método permite adicionar ou remover um item do vetor em uma dada posição. 
+  //res.render('pages/cadastro',{users:users});
+  console.log("Elementos cadastrados: ", users);
+  res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
 });
 
 // Inserir um usuário
-router.get('/cadastro/insert',(req,res)=>{
-    let usuario={name: "wellington", email: "wwagner@virtual.ufc.br"};
-    /* for(let cont=1;cont<=6;cont++){
-        usuarios.push({name:faker.name.findName(),email: faker.internet.email(),avatar: faker.image.image()});
-    } */
+router.get('/cadastro/insert', (req, res) => {
+  //substitui os valores armazenados no item do vetror dado por id, por valores fornecidos como parametro vindos do navegador.
+    //recebe dados do cliente na forma de um objeto JSON
 
-    let result = db.inserirDado(usuario);
-    console.log(result);
-    //res.render('pages/insert',{result});
+    users[req.body.id].name=req.body.name;
+    users[req.body.id].email=req.body.email;
+    users[req.body.id].address=req.body.address;
+    users[req.body.id].age=req.body.age;
+    users[req.body.id].height=req.body.height;
+    users[req.body.id].vote=req.body.vote;
+
+    console.log("Dados recebidos: ",req.body);//mostra no console do servidor os dados recebidos
+
+    res.sendStatus(200); //envia mensagem 200 significando que as modificacoes foram ok
 });
 
-router.get('/cadastro/list',(req,res)=>{
-//listar de usuarios cadastrado
+router.get('/cadastro/list', (req, res) => {
+  //listar de usuarios cadastrado
 });
 
 //Essa linha permite que este código seja exportado como um módulo e possa ser usado em outras partes da aplicação.
@@ -72,17 +70,20 @@ module.exports = router;
 
 
 
+/* Sobre o 'public' (routes.js, linha 12):
+   1. Especifica a pasta contendo arquivos estáticos;
+   2. O nome 'public' não precisará ser colocado na rota; 3. Para serem alcançados os arquivos e pastas que estão dentro dele;
+   4. Por isso na imagem que está na página home.ejs só há o indicativo para 'images'; */
 
+// forma não elegante de fazer: res.send
 
-
-
-/* Alguns exemplos de rotas: 
+/* Alguns exemplos de rotas:
    1. / = http://localhost:3030/
    2. /about = http://localhost:3030/about
    3. /curriculo =  http://localhost:3030/cadastro
 
 
-obs: ainda não sei porque as linhas a seguir não foram incluídas na rota dos usuários, vou descobrir (espero...) 
+obs: ainda não sei porque as linhas a seguir não foram incluídas na rota dos usuários, vou descobrir (espero...)
 
 router.get('/curriculo',(req,res)=>{
 res.send('Meu currículo');
