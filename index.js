32389d35da0bab35965ceffeddf82ffcbf8dcc04
Felipe <88596443+felipenferreira@users.noreply.github.com>
2021-08-13 02:11:38 +0000

    Mais uma alteração no código index.js, também para definir melhor as portas.

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const routes = require("./routes");
 
 const port = process.env.PORT || 3000;
-const address = "localhost";
+// const address = "localhost";
 // o local host é o nome padrão do computador e está ligado ao IP 127.0.0.1 (tcp-ip)
 
 app.use ('/', routes);
 
-const server = app.listen(port, address, () => {
-  let host = server.address().address;
+const server = app.listen(port, () => {
+  // let host = server.address().address;
   let port = server.address().port;
   console.log(`Servidor executando no endereço ${host} e porta ${port}`);
 });
